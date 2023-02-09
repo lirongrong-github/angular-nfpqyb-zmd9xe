42a66f6adf569c74a485dba403e57a6280734903
lirongrong-github <58406078+lirongrong-github@users.noreply.github.com>
2023-02-09 16:37:52 +0800

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { type Product } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  onClickShare(product: Product) {
+    console.log('product', product);
+    // window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
